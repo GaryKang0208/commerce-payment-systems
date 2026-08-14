74330bb634b3f7ba827f620a60d2b10d6b644d74
GaryKang0208 <khb1661@gmail.com>
2026-08-14 11:24:28 +0900

    Initial commit

---
+++ src/main/java/com/example/commercepaymentsystems/CommercePaymentSystemsApplication.java
@@ -0,0 +1,13 @@
+package com.example.commercepaymentsystems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommercePaymentSystemsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommercePaymentSystemsApplication.class, args);
+    }
+
+}
