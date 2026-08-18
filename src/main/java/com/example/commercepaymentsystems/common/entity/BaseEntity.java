fe99151afcdd71f4c70d407e15df15111820b44a
test <hello.git.GitHub@lomeafoer@gmail.com>
2026-08-18 16:52:13 +0900

    move: BaseEntity 디렉터리 구조 변경

--- src/main/java/com/example/commercepaymentsystems/common/entity/BaseEntity.java
+++ src/main/java/com/example/commercepaymentsystems/common/entity/BaseEntity.java
@@ -1,4 +1,4 @@
-package com.example.commercepaymentsystems.common;
+package com.example.commercepaymentsystems.common.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
