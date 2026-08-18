bea98c4a1e9220a9331b70b07dcd7e2b85e7aedd
조성연 <hiskybj000@gmail.com>
2026-08-18 14:56:10 +0900

    페잊지 설정 구현 완료

--- src/main/java/com/example/commercepaymentsystems/products/repository/ProductRepository.java
+++ src/main/java/com/example/commercepaymentsystems/products/repository/ProductRepository.java
@@ -4,7 +4,5 @@ import com.example.commercepaymentsystems.products.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product,Long> {
-    Long id(Long id);
 
-    Long id(Long id);
 }
