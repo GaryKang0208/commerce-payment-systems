e29efcc3abb7385d9ca8b4745c3b5dcdc89eb2d9
조성연 <hiskybj000@gmail.com>
2026-08-18 20:26:21 +0900

    카테고리 필터 구현 완료

---
+++ src/main/java/com/example/commercepaymentsystems/products/specification/ProductSpecification.java
@@ -0,0 +1,21 @@
+package com.example.commercepaymentsystems.products.specification;
+
+import com.example.commercepaymentsystems.products.entity.Product;
+import com.example.commercepaymentsystems.products.enums.ProductCategory;
+import com.example.commercepaymentsystems.products.repository.ProductRepository;
+import org.springframework.data.jpa.domain.Specification;
+
+public class ProductSpecification {
+    public static Specification<Product> hasCategory(ProductCategory category) {
+        return (root, query, criteriaBuilder) -> {
+            if (category == null) {
+                return null;
+            }
+            return criteriaBuilder.equal(
+                    root.get("category"),
+                    category
+            );
+        };
+    }
+}
+
