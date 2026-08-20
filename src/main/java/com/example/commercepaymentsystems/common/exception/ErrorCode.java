8611e105a8be5a8362a36cd41fe83c53fadfad43
taeheon <psw4497@gamil.com>
2026-08-20 16:31:27 +0900

    STOCK_EXCEEDED 재고 부족 에러 코드 추가

--- src/main/java/com/example/commercepaymentsystems/common/exception/ErrorCode.java
+++ src/main/java/com/example/commercepaymentsystems/common/exception/ErrorCode.java
@@ -30,7 +30,9 @@ public enum ErrorCode {
     //cart (CART_xxx)
     CART_EMPTY(HttpStatus.BAD_REQUEST, "CART_001", "장바구니가 비어있습니다." ),
     CART_ITEM_NOT_FOUND(HttpStatus.NOT_FOUND, "CART_002", "장바구니 항목을 찾을 수 없습니다."),
-    INVALID_QUANTITY(HttpStatus.BAD_REQUEST, "CART_003", "수량은 1 이상이어야 합니다.");
+    INVALID_QUANTITY(HttpStatus.BAD_REQUEST, "CART_003", "수량은 1 이상이어야 합니다."),
+    STOCK_EXCEEDED(HttpStatus.CONFLICT,"CART_004", "재고가 부족하여 담을 수 없습니다.");
+
 
     private final HttpStatus status;
     private final String code;
